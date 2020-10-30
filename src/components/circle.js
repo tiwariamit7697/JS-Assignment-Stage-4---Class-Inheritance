b205b743280141d57d1570c2eb514b0dcf874629
Amit <amitkumartiwari527@gmaial.com>
2020-10-30 19:10:15 +0530

    inheritance

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,18 @@
 // import using require
-
 // declare class
-
-
-// export class using module.exports
+//export class using module.exports
+//import Shape from './shape';
+const Shape=require('./shape.js');
+class Circle extends Shape
+{
+    constructor()
+    {
+        super();
+    }
+    calculateArea()
+    {
+        console.log("Area with color:"+this.color);
+        return 3.14*5*5;
+    }
+}
+module.exports=Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,19 @@
 // declare class
-
 // export class using module.exports
+class Shape
+{
+    constructor()
+    {
+        this.color="red";
+    }
+    drawShape()
+    {
+        console.log("draw any shape");
+    }
+    calculateArea()
+    {
+        console.log("unknown area");
+        return 0;
+    }
+}
+module.exports=Shape;
